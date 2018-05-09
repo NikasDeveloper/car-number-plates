a096c3eb6fcee68f723cd970b10404bd05379830
Nikas <nikas.lebedenko@gmail.com>
2018-05-09 11:54:02 +0300

    Added delete redux implementation in CarNumberPlate container

--- frontend/src/containers/CarNumberPlates/CarNumberPlate/CarNumberPlate.jsx
+++ frontend/src/containers/CarNumberPlates/CarNumberPlate/CarNumberPlate.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 import { connect } from 'react-redux';
-import { fetchCarNumberPlate } from "../../../store/actions/index";
+import {
+  fetchCarNumberPlate,
+  deleteCarNumberPlate,
+  modifyCarNumberPlateInit
+} from "../../../store/actions/index";
 import Aux from '../../../hoc/Aux';
 import DataPreloader from '../../../components/UI/Preloader/Data/DataPreloader';
 import Form from "../../../components/UI/Form/Form";
@@ -65,6 +70,11 @@ class CarNumberPlate extends Component {
     this.formSubmitHandler = event => {
       event.preventDefault();
       console.log('submited');
+    };
+    this.deleteClickedHandler = () => {
+      if ( window.confirm('Are you sure you want to delete this item?') ) {
+        this.props.onDelete(this.props.match.params.id);
+      }
     }
   }
 
@@ -74,18 +84,25 @@ class CarNumberPlate extends Component {
   }
 
   componentDidUpdate( prevProps, prevState, snapshot ) {
-    if ( this.props.carNumberPlate && !this.state.carNumberPlateFound ) this.carNumberPlateLoaded();
+    if ( this.props.carNumberPlate && !this.state.carNumberPlateFound ) {
+      this.carNumberPlateLoaded();
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.onModifyInit();
   }
 
   render() {
     let content = <DataPreloader/>;
-    if ( !this.props.loading ) {
+    if ( !this.props.loading && !this.props.modified ) {
       content = (
         <Form submitted={this.formSubmitHandler}>
           <FormGroup label="first name" error={this.state.inputs.firstName.error}>
             <Input name="firstName"
                    placeholder={this.state.inputs.firstName.placeholder}
                    value={this.state.inputs.firstName.value}
+                   required={this.state.inputs.firstName.required}
                    error={this.state.inputs.firstName.error}
                    changed={this.inputChangedHandler}/>
           </FormGroup>
@@ -93,6 +110,7 @@ class CarNumberPlate extends Component {
             <Input name="lastName"
                    placeholder={this.state.inputs.lastName.placeholder}
                    value={this.state.inputs.lastName.value}
+                   required={this.state.inputs.lastName.required}
                    error={this.state.inputs.lastName.error}
                    changed={this.inputChangedHandler}/>
           </FormGroup>
@@ -100,16 +118,23 @@ class CarNumberPlate extends Component {
             <Input name="number"
                    placeholder={this.state.inputs.number.placeholder}
                    value={this.state.inputs.number.value}
+                   required={this.state.inputs.number.required}
                    error={this.state.inputs.number.error}
                    changed={this.inputChangedHandler}/>
           </FormGroup>
           <div style={{ textAlign: 'right' }}>
-            <Button buttonType="button" style={{ marginRight: '10px' }}>Delete</Button>
+            <Button buttonType="button"
+                    style={{ marginRight: '10px' }}
+                    clicked={this.deleteClickedHandler}
+            >
+              Delete
+            </Button>
             <Button buttonType="submit" buttonClass="primary">Edit</Button>
           </div>
         </Form>
       );
     }
+    if ( this.props.modified ) content = <Redirect to="/car-number-plates"/>;
     return (
       <Aux>
         {content}
@@ -121,11 +146,15 @@ class CarNumberPlate extends Component {
 const mapStateToProps = state => ({
   carNumberPlate: state.carNumberPlate.carNumberPlate,
   loading: state.carNumberPlate.loading,
+  modifying: state.carNumberPlate.modifying,
+  modified: state.carNumberPlate.modified,
   error: state.carNumberPlate.error,
 });
 
 const mapDispatchToProps = dispatch => ({
-  onFetchStart: id => dispatch(fetchCarNumberPlate(id))
+  onFetchStart: id => dispatch(fetchCarNumberPlate(id)),
+  onDelete: id => dispatch(deleteCarNumberPlate(id)),
+  onModifyInit: () => dispatch(modifyCarNumberPlateInit()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CarNumberPlate);
