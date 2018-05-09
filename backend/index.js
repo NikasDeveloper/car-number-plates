f86cb9e2a1f8927098d9d03c7c0573792bd72c78
Nikas <nikas.lebedenko@gmail.com>
2018-05-09 20:18:46 +0300

    Removed production par in backend/index.js and added example env file

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
@@ -5,64 +6,55 @@ const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./../webpack.config.js');
 const mongoose = require('mongoose');
-const env = require('dotenv').config();
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
 const app = express();
-const morgan = require("morgan");
+const logger = require("morgan");
 const bodyParser = require("body-parser");
+const compiler = webpack(config);
+const middleware = webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: path.join('frontend', 'src'),
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  }
+});
 const carNumberPlateRoutes = require('./routes/carNumberPlates');
 
 mongoose.connect(process.env.DATABASE_URL);
-app.use(morgan("dev"));
+
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
+app.use(( req, res, next ) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if ( req.method === "OPTIONS" ) {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+app.use(middleware);
+app.use(webpackHotMiddleware(compiler));
+app.use('/api/car-number-plates', carNumberPlateRoutes);
+app.get('*', ( req, res ) => {
+  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '..', 'frontend', 'dist', 'index.html')));
+  res.end();
 });
 
-app.use('/api/car-number-plates', carNumberPlateRoutes);
-
-if (isDeveloping) {
-    const compiler = webpack(config);
-    const middleware = webpackMiddleware(compiler, {
-        publicPath: config.output.publicPath,
-        contentBase: path.join('frontend', 'src'),
-        stats: {
-            colors: true,
-            hash: false,
-            timings: true,
-            chunks: false,
-            chunkModules: false,
-            modules: false
-        }
-    });
-    app.use(middleware);
-    app.use(webpackHotMiddleware(compiler));
-    app.get('*', (req, res) => {
-        res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '..', 'frontend', 'dist', 'index.html')));
-        res.end();
-    });
-} else {
-    app.use(express.static(__dirname + './frontend/dist'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-    });
-}
-
 app.listen(port, error => {
-    if (error) {
-        return console.error(error);
-    }
-    console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+  if ( error ) {
+    return console.error(error);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
 });
